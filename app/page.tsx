ba98e818d2d13d2edf037a9c2a86cbb7fd582bf0
Sergiu <sergiu.lungu@opentech.email>
2024-08-05 19:16:42 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,64 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+const videos = [
+  "https://mega.nz/embed/bIk3iBbS#lFCtT2qjl7PxOPlMnJA5OHEo7WCk7HITWlL7GWUQtS0",
+  "https://mega.nz/embed/jA1VELyY#YmA8tnEXDCDplUluGXNXGuiYuy25iF2fJOyizkFD04s",
+  "https://mega.nz/embed/jUlWySob#EMuz84RTQxLJHXsJcUnPhV2luRkn4Nve__DCMwlwTfw",
+  "https://mega.nz/embed/7dMRRa7K#MRr9cbcu3hqy6va35oG_bdmN5ngNvi1mjxnE8sY_D-o",
+  "https://mega.nz/embed/jJdAhJaI#F1kqNHK_pWzVTupP5dU0ZO1yftG2TTn6cOsVEYg1e6U",
+  "https://mega.nz/embed/2Y8GGQBR#dIwSpgv7jfe4AOO3AOUk8MGIde3M5kbSkQWOPSAkJgw",
+  "https://mega.nz/embed/qQkWXR5R#1JRor8JxNVBM8KSuC1JCqDz-cW-lbPmTffNG41Gy6Ng",
+  "https://mega.nz/embed/Ld80Sb5Q#xdnOjYTz_udzRKHeBwe1Kf8OPoU_g0Ibfgpb6-_heGg",
+  "https://mega.nz/embed/mZcRnZAY#2m4sdSq6gp3mwX_jAJGsynxPUoFtIUsUqeF9phN-d4M",
+  "https://mega.nz/embed/uI0S1JoC#yxGYgcTfAQ8XdfJ7rQJ-i2YdiraJ8hMgtpHu1PW928E",
+  "https://mega.nz/embed/bENhkajK#OEozVmStlUQ6dGXvKu9OGMhfbqo9XBjiR4cwdIXZ3nE",
+  "https://mega.nz/embed/KF92yLDA#ZsQQrakYmlAMto8eiR2aDqZy3txX55A1ihSKbXHJbbE",
+  "https://mega.nz/embed/yR0WGIjZ#wwDgOp_b0BHeOLwCoP3FvRegfyQUKlG4YhCCvc4VMyI",
+  "https://mega.nz/embed/XEkClahL#pxYx3FEmqRr5pp9jemb_PJOejiSw8hX-pw2DU_D5iCE",
+  "https://mega.nz/embed/PAt1nS4D#SljrWsDwZvaV6QchsYnijdErtGsSVUqQKdA-VevrLwk",
+  "https://mega.nz/embed/CMEDFDJB#wYhdkADafHHU-FizKRl-ovfq7QgtuY4B7kVMX5SZEtI",
+  "https://mega.nz/embed/rUFy0ZRK#1vzOabfN7Z_Wowbai0d2ihqHfMlIS8L_5DhL_zJ_2as",
+  "https://mega.nz/embed/GB0WGTgI#dyLJljURiTiWn3aWyhd0LTZtMNrz_sgmmU1oc2tuQiMw",
+  "https://mega.nz/embed/vMsRhAQZ#uD1KWXMMREUqUv8cvpBCfhgSP_j5ry1RuyVHt0L9zGQ",
+  "https://mega.nz/embed/iYMGhS6L#xq--vf3cPsGxRdXATaBtjg9nY6ES8E16CyfRA2iuJus",
+  "https://mega.nz/embed/CV010AgA#XJek0mZYDbCxDlJa3sMXjYfn97E-yY0Wh4dlXZBvDQk",
+  "https://mega.nz/embed/bc1jiSJB#b_N1KdGMWUm55-zLD3cTA8oBgXT5d_ZmPFJRDQRFbfE",
+  "https://mega.nz/embed/6FUEWLQL#HPPIky3mrF9SLeceL_QMGG9aXKYy58PF7Ii1776FA_Y"
+];
 
 export default function Home() {
+  const [currentVideo, setCurrentVideo] = useState(videos[0]);
+
+  const changeVideo = () => {
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    setCurrentVideo(videos[randomIndex]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <iframe
+        width="1000"
+        height="500"
+        frameBorder="0"
+        src={currentVideo}
+        allowFullScreen
+      ></iframe>
+      <button
+        onClick={changeVideo}
+        style={{ marginTop: "20px", padding: "10px 20px", fontSize: "16px" }}
+      >
+        Сменить видео
+      </button>
+    </div>
   );
 }
