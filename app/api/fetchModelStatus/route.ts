226704438d7f6d6833d44b45fe67843b2c350e01
Sergiu <sergiu.lungu@opentech.email>
2025-03-09 22:52:40 +0200

    asdasd

--- app/api/fetchModelStatus/route.ts
+++ app/api/fetchModelStatus/route.ts
@@ -12,32 +12,3 @@ export async function GET() {
     return NextResponse.json(data);
   } catch (error) {}
 }
-
-export default async function handler(
-  req: { query: { modelName: any } },
-  res: {
-    status: (arg0: number) => {
-      (): any;
-      new (): any;
-      json: {
-        (arg0: { error?: string; html?: any }): void;
-        new (): any;
-      };
-    };
-  }
-) {
-  const { modelName } = req.query;
-  if (!modelName) {
-    res.status(400).json({ error: "Missing modelName parameter" });
-    return;
-  }
-  try {
-    const response = await fetch(`https://chaturbate.com/${modelName}`);
-    console.log(response)
-    // const html = await response.text();
-    // res.status(200).json({html});
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error fetching data" });
-  }
-}
